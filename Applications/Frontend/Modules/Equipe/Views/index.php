c060cfba3f80a8d7b0a724f94c16ade3d4582d68
ChanWenHao <android.philippechan@gmail.com>
2016-06-28 22:33:15 +0200

    Equipe->index
    *

--- Applications/Frontend/Modules/Equipe/Views/index.php
+++ Applications/Frontend/Modules/Equipe/Views/index.php
@@ -1,2 +1,29 @@
-<section>
+<div id="bloc-navi-theme">
+  <ul id="navi-theme">
+    <li class="theme">*
+      <svg><!--icone--></svg>
+      <h4>L'EQUIPE</h4>
+      <p></p>
+    </li>
+    <li class="theme">
+      <svg><!--icone--></svg>
+      <h4>MISSIONS</h4>
+      <p></p>
+    </li>
+    <li class="theme">
+      <svg><!--icone--></svg>
+      <h4>DONNEES</h4>
+      <p></p>
+    </li>
+
+</div>
+
+<section id="members-trombinoscope">
+<!--call-->
+  <div class="rolebox"></div>
+  <div class="trombibox"></div>
+</section>
+
+<section id="guest-trombinoscope">
+  <div class="trombibox"></div>
 </section>
