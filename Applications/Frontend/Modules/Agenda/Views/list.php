6ca852655309fbac97e474fde366e2b2e0d626de
ChanWenHao <android.philippechan@gmail.com>
2016-06-28 21:02:41 +0200

    Agenda-list
    *

--- Applications/Frontend/Modules/Agenda/Views/list.php
+++ Applications/Frontend/Modules/Agenda/Views/list.php
@@ -2,3 +2,17 @@
     <h1>GROS TITRE</h1>
     <p class="undertitle">paragraphe d'illustration</p>
 </section>
+
+<section id="event-list">
+  <!--call php-->
+  <ul class="event-month-item">
+    <!--call php-->
+    <li class="event-day-item"><!--call php-->
+        <a href="">
+          <span class="date"><!--call php--></span>
+          <h3><!--call php--></h3>
+          <p><!--call php--></p>
+        </a>
+    </li>
+  </ul>
+</section>
