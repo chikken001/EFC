5974a77d4251bfb6e4e8055f662c5636fda026f5
ChanWenHao <android.philippechan@gmail.com>
2016-07-03 20:58:59 +0200

    index.style.img
    push

--- Applications/Frontend/Modules/Accueil/Views/index.php
+++ Applications/Frontend/Modules/Accueil/Views/index.php
@@ -3,45 +3,141 @@
 	<div class="wrapper">
 
 			<section id="main-landing-element">
-					<h2>MINI TITRE</h2>
-					<h1>GROS TITRE</h1>
-					<p class="undertitle">paragraphe d'illustration des mini et gros titre</p>
-					<a class="boutton-contact">
-						<span>NOUS CONTACTER</span>
-					</a>
+					<h2>ASSOCIATION EFC</h2>
+					<h1>L'environnement, au coeur de notre action</h1>
+					<p class="undertitle"></p>
+					<div class="boutton-contact">
+						NOUS CONTACTER
+					</div>
 			</section>
 
-			<section class="catch-elements">
 					<div id="black-block">
-						<h2></h2>
+						<h2>EGET MOLESTIE LOBORTIS, EST LACUS VEHICULA MAGBNA, VITAE FAUCIBUS EX TUPIS SUT AMEL ELIT PHASELLUS NON PRETIUM LUGULA</h2>
 						<ul class="social-icons">
 								<li><img src="url_linkedin"/></li>
 								<li><img src="url_tweet"/></li>
 								<li><img src="url_googplus"/></li>
 						</ul>
 					</div>
-					<article id="mini-prez-1">
-							<figure class="underlayer">
-									<img src=""/>
-									<figcaption></figcaption>
-							</figure>
-							<em>.02</em>
-							<h2></h2>
-							<p></p>
-							<a class="boutton-continuer">
-								<span>CONTINUER</span>
-							</a>
-					</article>
-			</section>
+
+							<div class="underlayer">
+
+							</div>
+
+      <div class="main-center-elements">
+        <figcaption>MISSION</figcaption>
+              <article class="mini-prez-1">
+    							<em>.01</em>
+    							<h2>MAURIS SIT AMET GRAVIDA<br/> DONECT PORTA NISI</h2>
+    							<p><span>E</span>get molestie lobortis, est lacus vehicula magna,
+                    vitae faucibus ex turpis sit amet elit.non,
+                    sagittis hendrerit orci.
+                    Morbi id euistempor eget ac lectus non premi Fusce a congue nunc.
+                    Aenean risus nulla,</p>
+    							<a class="boutton-continuer">
+    								<span>CONTINUER</span>
+    							</a>
+					    </article>
 
 			<section id="article-sample">
-				<!--php call-->
-				<div class="item-article">
-					<img src=""/>
-					<h3></h3>
-					<p></p>
-				</div>
+        <figcaption>ACTUALITES</figcaption>
+
+        <!--php call-->
+        <div id="box-item-article">
+  				<div class="item-article">
+
+    					<img src="/web/img/8265922-12928727.jpg">
+              <i>
+                <a href="#">></a>
+              </i>
+              </img>
+
+  					<h3>KOF 2016 CONFIMENTUM</h3>
+  					<p><span>D</span>ictum vel augue non, sagittis hendrerit orci. Morbi id euistempor eget ac lectus non premi
+...</p>
+  				</div>
+
+          <!--item test-->
+          <div class="item-article">
+  					<img src="/web/img/photo-1440940627368-bd68a386bb73.jpg">
+            <i>
+              <a href="#">></a>
+            </i>
+            </img>
+  					<h3>KOF 2016 CONFIMENTUM</h3>
+  					<p><span>D</span>ictum vel augue non, sagittis hendrerit orci. Morbi id euistempor eget ac lectus non premi
+...</p>
+  				</div>
+          <div class="item-article">
+  					<img src="/web/img/photo-1417144527634-653e3dec77b2.jpg">
+            <i>
+              <a href="#">></a>
+            </i>
+            </img>
+  					<h3>KOF 2016 CONFIMENTUM</h3>
+  					<p><span>D</span>ictum vel augue non, sagittis hendrerit orci. Morbi id euistempor eget ac lectus non premi
+...</p>
+  				</div>
+          <!--fin item test-->
+        </div>
 			</section>
+
+      <section id="agenda-sample">
+        <figcaption>AGENDA</figcaption>
+
+        <div id="box-item-agenda">
+          <div class="item-agenda-date-top">
+            <time>mai</time>
+            <time><strong>27</strong></time>
+          </div>
+          <div class="item-agenda-text-top">
+            <h3>EGET MAGNA LOPSEM MOLESTIE</h3>
+            <p><span>i</span>n tempor diam. Fusce a congue nunc. Aenean risus nulla</p>
+          </div>
+          <div class="item-agenda-date">
+            <time>mai</time>
+            <time><strong>27</strong></time>
+          </div>
+          <div class="item-agenda-text-top">
+            <h3>EGET MAGNA LOPSEM MOLESTIE</h3>
+            <p><span>i</span>n tempor diam. Fusce a congue nunc. Aenean risus nulla</p>
+          </div>
+          <div class="item-agenda-date">
+            <time>mai</time>
+            <time><strong>27</strong></time>
+          </div>
+          <div class="item-agenda-text-top">
+            <h3>EGET MAGNA LOPSEM MOLESTIE</h3>
+            <p><span>i</span>n tempor diam. Fusce a congue nunc. Aenean risus nulla</p>
+          </div>
+        </div>
+      </section>
+
+      <section id="contact-sample">
+        <figcaption>CONTACT</figcaption>
+
+        <article class="mini-prez-1">
+            <em>.02</em>
+            <h2>MAURIS SIT AMET GRAVIDA<br/> DONECT PORTA NISI</h2>
+            <p><span>E</span>get molestie lobortis, est lacus vehicula magna,
+              vitae faucibus ex turpis sit amet elit.non,
+              sagittis hendrerit orci.
+              Morbi id euistempor eget ac lectus non premi Fusce a congue nunc.
+              Aenean risus nulla,</p>
+            <a class="boutton-continuer">
+              <span>CONTINUER</span>
+            </a>
+        </article>
+
+      </section>
+
+      <figcaption id="contact-caption">CONTACT</figcaption>
+
+    </div>
+
+    <div class="underlayer-bis">
+
+    </div>
 
 	</div>
 
